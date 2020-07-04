2d46081bdc4fc079283385f3fb4fc98014d28838
amanjotsinghdhunna <amanjot.singh@successive.tech>
2020-07-04 18:56:22 +0530

    added google analytic

--- src/ga.js
+++ src/ga.js
@@ -2,7 +2,9 @@ import ReactGA from 'react-ga';
 
 (function initializeReactGA() {
     const gaId = process.env.REACT_APP_GAID
+    console.log(process.env)
     if (!gaId) return;
     ReactGA.initialize(gaId);
     ReactGA.pageview('/');
 })();
+
