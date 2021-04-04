16063130549d68ad5388283d3074b6ab59740c37
amanjotsinghdhunna <amanjotsingh260@gmail.com>
2021-04-04 14:08:27 +0530

    added inbuild resime using iframe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter,Switch } from 'react-router-dom'
 import Main from "./home/Home";
 import About from "./about/About";
 import Contact from "./contact/Contact";
+import resume from "./assets/Resume.pdf";
 import "./App.css";
 
 // google analytics
@@ -12,10 +13,17 @@ function App() {
   return (<>
     <a
       className="download-btn"
-      href="https://drive.google.com/file/d/1bfcp9no13ONLMbQowGJA6iaX15uoxM4x/view?usp=sharing"
-      download="resume_amanjotSingh"
+      href={resume}
+      download="resume_amanjotSinghDhunna"
+      target="_blank"
+      rel="noopener noreferrer"
       ><div>CV</div>
     </a>
+    <div ></div>
+    <object className="resumeObject" data={resume} type="application/pdf">
+        {/* <embed src={resume} type="application/pdf" /> */}
+        <iframe src={resume}></iframe>
+    </object>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Main} />
