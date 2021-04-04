d8d1a75a2eb9226f5cbe57926e01660feba25a07
amanjotsinghdhunna <amanjotsingh260@gmail.com>
2021-04-04 13:29:05 +0530

    added underline text decorator to h2 em and removed warings

--- src/about/About.js
+++ src/about/About.js
@@ -1,50 +1,6 @@
-import React, {useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../header/Header';
 import './about.css';
-
-const randomColor = () => {
-    const getDefaultColor = () => {
-      const randomColor = [
-        "#52BF84",
-        "#FEB957",
-        "#DD615D",
-        "#EF7DD0",
-        "#59C1F4",
-        "#F0638E",
-        "#1AABA2",
-        "#5F7AE8",
-        "#FD7554",
-        "#9D83F4",
-      ];
-      const randomGenerator = randomColor[Math.ceil(Math.random() * 10) -1 ];
-      return randomGenerator;
-    };
-    return { background: getDefaultColor() };
-};
-const randomGrid = () => {
-        const defaultGridClasses = [ 'one', 'two', 'three', 'four', 'five', 'six' ];
-        const randomGenerator = Math.ceil(Math.random() * 10)%6;
-        return defaultGridClasses[randomGenerator];
-}
-
-const getColors = () => ({
-    wrapper: { background: "#000" },
-    one: randomColor(),
-    two: randomColor(),
-    three: randomColor(),
-    four: randomColor(),
-    five: randomColor(),
-    six: randomColor(),
-  });
-
-const getGrid =() => ({
-    one: randomGrid(),
-    two: randomGrid(),
-    three: randomGrid(),
-    four: randomGrid(),
-    five: randomGrid(),
-    six: randomGrid(),
-});
 
 function About() {
    
